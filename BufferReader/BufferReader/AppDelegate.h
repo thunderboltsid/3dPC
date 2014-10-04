384dff8db86ee2abc35f0dadb5b7ddd19f2430b1
Alexandru Barbarosie <abarbarosie@Alexandrus-Mac-mini.local>
2014-10-04 13:55:41 +0200

    Initial Commit

---
+++ BufferReader/BufferReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BufferReader
+//
+//  Created by Alexandru Barbarosie on 04/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BufferReader/BufferReader/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  BufferReader
+//
+//  Created by Alexandru Barbarosie on 04/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
