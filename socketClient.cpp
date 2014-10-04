966576a4c29e378c6bcff19d06de3656177404f2
Toloaca Ion <i.toloaca@jacobs-university.de>
2014-10-04 18:57:10 +0200

    whatever

--- socketClient.cpp
+++ socketClient.cpp
@@ -30,23 +30,20 @@ std::mutex mtx;
 void client_routine() {
     mtx.lock();
     SocketClient::getInstance().connect_socket("localhost",4242);
-//    SocketClient::getInstance().send<char>("abc",3);
-//    int* p  = new int[3];
-//    SocketClient::getInstance().send<int>(p,3);
-//    char* buffer = new char[30];
-//    SocketClient::getInstance().receive<char>(buffer, 29);
     int* valPtr = new int[1];
     SocketClient::getInstance().receive<int>(valPtr,1);
     int length = valPtr[0];
     printf("Lenght: %d\n",length);
-    
+
     unsigned char *buf = new unsigned char [length];
-    SocketClient::getInstance().receive<unsigned char>(buf,length);
-    
+    while (1) {
+        SocketClient::getInstance().receive<unsigned char>(buf,length);
+        SocketClient::getInstance().receive<int>(valPtr,1);
+    }
     delete [] buf;
     delete valPtr;
     mtx.unlock();
-    sleep(20);
+
 }
 
 int main ()
@@ -54,3 +51,4 @@ int main ()
     std::thread curr_thread(client_routine);
     curr_thread.join();
 }
+
